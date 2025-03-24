0253d5d856a05e04757764b163676f5f3d4d8295
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-24 15:25:27 -0300

    feat: add id to gender checkboxes for better selection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var name = document.querySelector("exampleInputName").value;
-var man = document.querySelector("exampleInputGenderM").value;
-var woman = document.querySelector("exampleInputGenderF").value;
-var birth = document.querySelector("exampleInputBirth").value;
-var Country = document.querySelector("exampleInputCountry").value;
-var email = document.querySelector("exampleInputEmail1").value;
-var password = document.querySelector("exampleInputPassword1").value;
-var photo = document.querySelector("exampleInputFile").value;
+var name = document.querySelector("exampleInputName");
+var agender = document.querySelectorAll("form-user-create [name=agender]:checked");
+var birth = document.querySelector("exampleInputBirth");
+var Country = document.querySelector("exampleInputCountry");
+var email = document.querySelector("exampleInputEmail");
+var password = document.querySelector("exampleInputPassword");
+var photo = document.querySelector("exampleInputFile");
+var admin = document.querySelector("exampleInputAdmin");
