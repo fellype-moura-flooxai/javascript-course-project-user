b518af49af09872c4c78b25092c75162e02fb89d
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-25 18:25:19 -0300

    style: ternary operator for conditional rendering in user table layout

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,25 +17,32 @@ class UserController {
 
             let values = this.getValues();
 
-            values.photo = "";
-            
-            this.getPhoto((content)=>{
+            this.getPhoto().then(
+              (content)=> {
 
-                values.photo = content;
+                    values.photo = content;
 
-                this.addLine(values);
-    
-            });
+                    this.addLine(values);
+
+            },
+             (e)=> {
+                console.error(e);
+            }
+        );
+        
         
         });
 
     }
 
-    getPhoto(callback){
+    getPhoto(){
+
+
+        return new Promise((resolve, reject)=>{
 
         let fileReader = new FileReader();
 
-        let elements = [...this.formEl.elements].filter(item=>{
+        let elements = [...this.formEl.elements].filter(item=> {
 
             if (item.name === 'photo') {
                 return item;
@@ -44,14 +51,27 @@ class UserController {
 
      let file = elements[0].files[0];
 
-        fileReader.onload = ()=>{
+        fileReader.onload = () => {
 
-            callback(fileReader.result);
+            resolve(fileReader.result);
 
         };
 
-        fileReader.readAsDataURL(file);
-    };
+        fileReader.onerror = (e)=>{
+
+            reject(e);
+
+        }
+
+        if (file) {
+             fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+                
+
+        });
+}
 
     getValues(){
 
@@ -64,6 +84,11 @@ class UserController {
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
+        
+            } else if (field.name == "admin") {
+
+                user[field.name] = field.checked;
+                
         
             } else {
         
@@ -87,20 +112,22 @@ class UserController {
     }
 
     addLine(dataUser){
-    
-       this.tableEl.innerHTML = `
-        <tr>
+
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
           <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
           <td>${dataUser.name}</td> 
           <td>${dataUser.email}</td>
-          <td>${dataUser.admin}</td>
+          <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
           <td>${dataUser.birth}</td>
           <td>
           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
           </td>
-        </tr>
      `;
+    
+       this.tableEl.appendChild(tr);
     
     }
 
